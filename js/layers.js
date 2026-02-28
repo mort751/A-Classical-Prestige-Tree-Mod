cca34c5b1a962474919ae6956e85ede36d4a001b
mort751 <ayodejitaku@gmail.com>
2026-02-28 22:06:44 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -3,7 +3,7 @@ addLayer("p", {
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "#138cdc",
@@ -45,5 +45,36 @@ addLayer("p", {
         effect() { return player.p.points.add(1).sqrt().add(1) },
         effectDisplay() { return format(this.effect()) + "x" }
     },
+    }
+})
+
+addLayer("f", {
+    name: "flux", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#dc8213",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "time flux", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.25, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "f", description: "F: Reset for time flux.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return player.p.unlocked || player[this.layer].unlocked},
+    upgrades: {
     }
 })
