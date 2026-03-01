2c3dfd84e443a989c6c085f464e61f451e497cb9
mort751 <ayodejitaku@gmail.com>
2026-03-01 09:47:11 +0000

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -44,6 +44,7 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if(hasUpgrade('p', 12)) gain = gain.mul(3)
 	if(hasUpgrade('p', 13)) gain = gain.mul(upgradeEffect('p', 13))
+	if(hasUpgrade('p', 31)) gain = gain.mul(upgradeEffect('p', 31))
 	if(player.f.unlocked && player.f.activeTime.gt(0)) gain = gain.mul(tmp.f.effect)
 	return gain
 }
