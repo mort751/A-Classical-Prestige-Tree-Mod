e54b1c34a6efc77f4fbbaf8a5dd6943d7cd2c433
mort751 <ayodejitaku@gmail.com>
2026-03-01 09:48:04 +0000

    Update NumberFormating.js

--- js/utils/NumberFormating.js
+++ js/utils/NumberFormating.js
@@ -80,8 +80,8 @@ function formatWhole(decimal) {
 }
 
 function formatTime(s) {
-    if (s < 60) return format(s) + "s"
-    else if (s < 3600) return formatWhole(Math.floor(s / 60)) + "m " + format(s % 60) + "s"
+    if (s < 60) return format(s) + " seconds"
+    else if (s < 3600) return formatWhole(Math.floor(s / 60)) + " mins " + format(s % 60) + " secs"
     else if (s < 86400) return formatWhole(Math.floor(s / 3600)) + "h " + formatWhole(Math.floor(s / 60) % 60) + "m " + format(s % 60) + "s"
     else if (s < 31536000) return formatWhole(Math.floor(s / 86400) % 365) + "d " + formatWhole(Math.floor(s / 3600) % 24) + "h " + formatWhole(Math.floor(s / 60) % 60) + "m " + format(s % 60) + "s"
     else return formatWhole(Math.floor(s / 31536000)) + "y " + formatWhole(Math.floor(s / 86400) % 365) + "d " + formatWhole(Math.floor(s / 3600) % 24) + "h " + formatWhole(Math.floor(s / 60) % 60) + "m " + format(s % 60) + "s"
